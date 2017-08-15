c1ae79494a9b1f91137c3deaaaa177950e2c4f42
solidtavo <A01375762>
2017-08-15 15:40:41 -0500

    Commit inicial 15/08/2017

---
+++ Tarea1/Perimetro rect.cpp
@@ -0,0 +1,25 @@
+/*********************************************************
+Materia: Gráficas Computacionales
+Fecha: 16 de agosto del 2017
+Autor: A01375762 Octavio Adrian Valencia Ballesteros
+*********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int b=5;
+	int a = 3;
+	int p;
+
+	cout<<endl<<"Este programa calcula el PERIMETRO DE UN RECTANGULO de base "<<b<<" y altura "<<a<<endl<<endl;
+	p = b*a;
+	cout << "     PERIMETRO= " << p << endl<<endl<<endl;
+	cout << "Presione ENTER para salir";
+	cin.get();
+
+	return 0;
+}
